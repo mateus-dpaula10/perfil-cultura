1ecd911c75a3bdea328a998b52d25f1a9a5ba612
Mateus De Paula <mateus.dpaula10@gmail.com>
2025-07-28 10:36:15 -0300

    Adiciona ico à aplicação

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,6 +6,7 @@
     <title>Perfil de cultura - @yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="icon" type="image/png" href="{{ asset('img/logos/ico-colorido.png') }}">
 </head>
 <body id="dashboard">
     <div>
